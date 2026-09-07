3e9fd39434c00e7aa01c64a429810aa83bbd5427
MAHALAKSHMI KOMMARANAHALLI <mahalakshmikommaranahalli9@gmail.com>
2026-09-07 23:35:30 +0530

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -3,34 +3,52 @@ const CACHE_NAME = "memoryverse-v1";
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
 
     event.waitUntil(
+
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(cache => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+
     );
 
     self.skipWaiting();
+
 });
 
 
 self.addEventListener("activate", event => {
 
     event.waitUntil(
+
         caches.keys()
-            .then(keys =>
-                Promise.all(
-                    keys
-                        .filter(key => key !== CACHE_NAME)
-                        .map(key => caches.delete(key))
-                )
-            )
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys.map(key => {
+
+                        if (key !== CACHE_NAME) {
+                            return caches.delete(key);
+                        }
+
+                    })
+
+                );
+
+            })
+
     );
 
     self.clients.claim();
+
 });
 
 
@@ -39,9 +57,11 @@ self.addEventListener("fetch", event => {
     event.respondWith(
 
         caches.match(event.request)
-            .then(response =>
-                response || fetch(event.request)
-            )
+            .then(response => {
+
+                return response || fetch(event.request);
+
+            })
 
     );
 
